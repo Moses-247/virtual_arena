f3ed6fed706bf8a6c471ed2736fb9e730994a67e
Moses-247 <asugam9@gmail.com>
2025-02-19 14:28:37 +0300

    cloudinary

--- backend/src/contollers/auth.controller.js
+++ backend/src/contollers/auth.controller.js
@@ -76,7 +76,7 @@ export const login = async (req, res) => {
     }
 };
 
-export const logout = (req, res) => {
+export const logout = async (req, res) => {
     try {
         res.cookie("jwt", "", {maxAge : 0});
         res.status(200).json({ message: "Logged out succesfully" });
@@ -84,4 +84,8 @@ export const logout = (req, res) => {
         console.log("error in logout controller", error.message);
         res.status(500).json({message: "Internal Server Error"});
     }
+};
+
+export const updateProfile = async(res,req) => {
+    
 };
