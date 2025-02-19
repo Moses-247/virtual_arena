37f28d99c3ba6736d1d2c91ffa00a2939fb9e4d3
Moses-247 <asugam9@gmail.com>
2025-02-19 17:29:38 +0300

    setup frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from './lib/db.js';
 
 const app = express();
@@ -13,7 +14,8 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () =>{
     console.log("Server is running on port:" + PORT);
