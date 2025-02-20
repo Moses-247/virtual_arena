9b9b47a1c10d36e14c80b24af99601f757b59365
Moses-247 <asugam9@gmail.com>
2025-02-20 03:13:25 +0300

    setup frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import Navbar from "./components/Navbar";
+
+import Homepage from "./pages/Homepage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+
+import { Routes, Route } from "react-router-dom";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+
+
+import { Loader } from "lucide-react";
 
 const App = () => {
+  const {authUser, checkAuth, isCheckingAuth} =useAuthStore();
+
+  useEffect(() =>{
+    checkAuth()
+  }, [checkAuth]);
+
+  console.log({ authUser });
+
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+
   return (
-    <div className='text-red-500'>Hello Kenyan</div>
+    <div>
+
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+
+      </Routes>
+
+    </div>
   )
 }
 
